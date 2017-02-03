c032bc35e653416ef02947c56d4c4d211f7eae89
Darc Boykin <darcboykin@gmail.com>
2017-02-03 17:27:11 -0500

    add the user login feature

---
+++ auth/local.js
@@ -0,0 +1,31 @@
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+
+const init = require('./passport');
+const models = require('../db/models/index');
+const authHelpers = require('../auth/auth-helper');
+
+const options = {};
+
+init();
+
+passport.use(new LocalStrategy(options, (username, password) => {
+  models.Users.findOne({
+    where: {
+      username
+    }
+  }).then((user)=> {
+    console.log(user);
+    if (!user) {
+      return done(null, false);
+    }
+    if (!authHelpers.comparePass(password, user.dataValues.password)) {
+      return done(null, false);
+    } else {
+      return done(null, user.dataValues);
+    }
+  })
+  .catch((err) => { return done(err); });
+}));
+
+module.exports = passport;
