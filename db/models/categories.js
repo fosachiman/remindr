4921e721b8b5f22308ebdef8984f0a12d32d6760
fosachiman <jasonphumphreys@gmail.com>
2017-02-03 10:02:56 -0500

    added categories table

---
+++ db/models/categories.js
@@ -0,0 +1,14 @@
+'use strict';
+module.exports = function(sequelize, DataTypes) {
+  var Categories = sequelize.define('Categories', {
+    user_id: DataTypes.INTEGER,
+    category: DataTypes.STRING(255)
+  }, {
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+      }
+    }
+  });
+  return Categories;
+};
