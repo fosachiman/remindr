2afb4565dde1f45d094f84ae0c88ff40cc4cbdf5
fosachiman <jasonphumphreys@gmail.com>
2017-02-06 09:29:27 -0500

    data pulling from server to user page

--- users/user-helpers.js
+++ users/user-helpers.js
@@ -32,26 +32,7 @@ function getCategories(req, res, next) {
   });
 }
 
-// function organizeInfo(req, res, next) {
-//   // models[res.locals.userInfo].findAll({
-//   //   attributes: 'name'
-//   // }).then((categories) => {
-//   //   console.log(models[res.locals.userInfo]);
-//   // });
-//   // next();
-// }
 
-// function combineUserInfo(req, res, next) {
-//   models.sequelize.query(`SELECT "Users"."name", "Items"."name", "Items"."desc", "Items"."category", "Items"."suggestion", "Friends"."user_1", "Friends"."user_2" FROM "Users" JOIN "Categories" ON "Users"."email" = "Categories"."email" JOIN "Items" ON "Users"."id" = "Items"."user_id" JOIN "Friends" ON "Users"."id" = "Friends"."user_1" WHERE "Users"."id" = ${req.params.id}`, {
-//     type: models.sequelize.QueryTypes.SELECT // don't need metadata in the response
-//   }).then((userInfo) => {
-//     console.log(userInfo);
-
-//     res.locals.userInfo = userInfo; // setting res.locals object to access in the response
-//     console.log(res.locals.userInfo);
-//     return next(); // next function
-//   });
-// }
 
 module.exports = {
   validateUser,
