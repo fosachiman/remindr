471f5ad29871aacd89745f8f1027f79759e1f094
patrice prosper <new>
2017-02-07 15:10:28 -0500

    edit and delete within categories drop down full functional

--- public/javascripts/editItem.js
+++ public/javascripts/editItem.js
@@ -25,8 +25,8 @@ $( document ).ready(function() {
     var form = "<form class=\"inline-editor\">";
     form += "<input type=\"text\" class=\"editBox\" value=\"";
     form += oldText;
-    form += "\" /> </form>";
-    form += "<a href=\"#\" class=\"btnSave white-text btn\">Save</a>";
+    form += "\" </form>";
+    form += "<a href=\"#\" class=\"btnSave white-text btn\"> Save</a>";
     form += "<a href=\"#\" class=\"btnDiscard\"> <span> Cancel </span> </a>";
     return form
   }
@@ -66,59 +66,3 @@ $( document ).ready(function() {
 
   }
   });
-
-
-// $(function() {
-//   // $.getJSON('api', updateItem);
-//   //
-//   // $('.addItems').submit(function(e) {
-//   //   e.preventDefault();
-//   //   $.post('api', {
-//   //     item: $('#item-form-name').val(),
-//   //   }, updateItem);
-//   // });
-//
-//   $(".inline-editable").bind("click", timeout)
-//
-//   $('.inline-editable').on('click', function delete(e) {
-//       if (e.target.className == '    ') {
-//         $.ajax({
-//           url: 'api/' + e.target.id,
-//           type: 'DELETE',
-//           success: updateItem
-//         }); //ajax
-//         setTimeout(function(){
-//         alert(" Your item will delete " + score ); }, 30000)
-//       } // the target is a delete button
-//   }); //feedback messages
-//
-// $(".close-green").click(function () {
-//         $("#message-green").fadeOut("slow");
-//     });
-//
-//     setTimeout(function(){
-//         $("#message-green").fadeOut("slow");
-//     },5000)
-//
-//
-//   function updateItem(data) {
-//    var output = '';
-//    $.each(data,function(key, item) {
-//      output += '     <div class="feedback-item item-list media-list">';
-//      output += '       <div class="media-left">
-//                           <button class="feedback-delete btn btn-xs btn-danger">
-//                             <span id="' + key + '" class="  "></span>
-//                           </button>
-//                         </div>';
-//     //  output += '           <div class="feedback-head">';
-//      output += '         <div class="inline-editable">' + item.title
-//                             + ' <small class="feedback-name label label-info">'
-//                             + 'Suggest Item'+ '</small>
-//                         </div>';
-//      output += '     </div>';
-//    });
-//    $('.inline-editable').html(output);
-//   }
-// });
-//
-// });
