fb3cbe06603973f69fe59088901a039745424ee3
fosachiman <jasonphumphreys@gmail.com>
2017-02-07 14:24:54 -0500

    hp reg and userpage styles and dom table manipulation - add button

--- public/javascripts/userpage.js
+++ public/javascripts/userpage.js
@@ -2,16 +2,39 @@
 
 //ADD
 
-let input = document.querySelector('#add-text');
+let input = document.querySelector('#add-item-text');
 let button = document.querySelector('#add-button');
 button.addEventListener('click', addClickHandler);
 
 function addClickHandler() {
-  let newThing = document.createElement('p');
-  let list = document.querySelector('#list');
+  let newThing = document.createElement('div');
+  let list = document.querySelector('#item-list');
+  let addBox = document.querySelector('#add-item-text')
+  // let parent = list.parentElement;
   list.appendChild(newThing);
-  newThing.innerHTML = input.value;
+  let newItem = document.createElement('input');
+  newItem.setAttribute('class', 'newItem col s10 list-item');
+  newItem.setAttribute('type', 'text');
+  newItem.setAttribute('name', 'item');
+  newItem.setAttribute('value', addBox.value);
+  let trash = document.createElement('i');
+  trash.setAttribute('class', 'material-icons col s1 delete-button');
+  trash.setAttribute('id', 'delete-button');
+  trash.innerHTML = 'delete';
+  let desc = document.createElement('i');
+  desc.setAttribute('class', 'material-icons col s1 desc-button');
+  desc.setAttribute('id', 'desc-button');
+  desc.innerHTML = 'info';
+  newThing.appendChild(newItem);
+  newThing.appendChild(trash);
+  newThing.appendChild(desc);
   newThing.setAttribute('style', 'background-color: green');
+  resetAddBox();
+}
+
+function resetAddBox() {
+  let addBox = document.querySelector('#add-item-text');
+  addBox.value = '';
 }
 
 
@@ -29,4 +52,35 @@ function deleteClickHandler(e) {
   else
     item.style.textDecoration = 'line-through';
 }
+
+// <ul class="collapsible box-thing" data-collapsible="accordion">
+//     <% categories.forEach((category) => {%>
+//     <li category="<%= category.dataValues.category %>" id="<%= category.dataValues.category %>">
+//       <div class="collapsible-header category-item"><%= category.dataValues.category %></div>
+//       <div class="collapsible-body row">
+//       <input id="add-item-text" type="text" placeholder="add a thing" class="add-field input-field col s10">
+//       <div class="col s2 add-button-cont">
+//        <a id="add-button" class="btn-floating btn-large waves-effect waves-light add-button"><i class="material-icons">add</i></a>
+//        </div>
+//       <form action="<%='/users/' + user.dataValues.id +'/' + category.dataValues.category %>" method="post">
+//         <ul id="item-list" class="<%= category.dataValues.category %> newItemField" tether="<%= category.dataValues.category %>">
+//           <% items.forEach((item) => { %>
+//           <% if (item.dataValues.category === category.dataValues.category && item.dataValues.name !== '') { %>
+//             <div>
+//               <input type="text" name="item" class="newItem col s10 list-item" id="<%= item.dataValues.name %>_input" value="<%= item.dataValues.name %>">
+//               <i class="material-icons col s1 delete-button" id="delete-button">delete</i>
+//               <i class="material-icons col s1 desc-button" id="desc-button">info</i>
+//             </div>
+//           <%  } }) %>
+//           <!-- <button class="btn waves-effect waves-light" type="submit" name="action">Save
+//             <i class="material-icons right">send</i>
+//           </button> -->
+//         </form>
+//         </ul>
+//       </div>
+//     </li>
+//     <% }) %>
+// </ul>
+
+// item description
 
--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ var models = require('../db/models/index');
 router.get('/', function(req, res, next) {
   res.render('index', {
     title: 'Remindr',
-    slogan: 'Never waste an wasted moment'
+    slogan: 'Never waste an wasted moment again'
   });
 });
 
@@ -28,36 +28,11 @@ router.post('/register', regHelper.createCategories, (req, res, next)  => {
   .catch((err) => { res.status(500).json({ status: err }); });
 });
 
+router.get('/logout', (req, res) => {
+  req.logout();
+  res.redirect('/');
+});
+
 
 module.exports = router;
 
-
-//THIS IS HOW THINGS WILL BE JOINED >>>
-
-// Project.belongsToMany(Task)
-// Task.belongsToMany(Project)
-
-// Project.create()...
-// Task.create()...
-// Task.create()...
-
-// // save them... and then:
-// project.setTasks([task1, task2]).then(function() {
-//   // saved!
-// })
-
-// // ok, now they are saved... how do I get them later on?
-// project.getTasks().then(function(associatedTasks) {
-//   // associatedTasks is an array of tasks
-// })
-
-// // You can also pass filters to the getter method.
-// // They are equal to the options you can pass to a usual finder method.
-// project.getTasks({ where: 'id > 10' }).then(function(tasks) {
-//   // tasks with an id greater than 10 :)
-// })
-
-// // You can also only retrieve certain fields of a associated object.
-// project.getTasks({attributes: ['title']}).then(function(tasks) {
-//     // retrieve tasks with the attributes "title" and "id"
-// })
