a73cc9f443b8c110e272c26bcd10993bc8a2c384
patrice prosper <new>
2017-02-07 03:39:24 -0500

    added for each loop on rendered data and materialize delete button

--- public/javascripts/deleteItem.js
+++ public/javascripts/deleteItem.js
@@ -2,8 +2,8 @@ $(".submit").on("click", function(e) {
    e.preventDefault();
 
    //pull new note from html form input & trim for easier comparison
-   var newitem = $(".dataItem").val().trim();
-   $(".dataItem").each(function() {
+   var newitem = $(".inline-editable").val().trim();
+   $(".inline-editable").each(function() {
      if($(this).text().trim().toLowerCase() === [0].toLowerCase()) {
        return;
      }
@@ -12,20 +12,20 @@ $(".submit").on("click", function(e) {
    //after checking for the duplicates, if duplicate, alert! Then slideback
 
    //adding newNote into the exisiting table
-   var newRow = $("<tr>");
-   var noteTd = $("<td>").addClass("note-td").append(newitem);
+   var newRow = $("<li>");
+   var noteTd = $("<li>").addClass("note-td").append(newitem);
    var deleteBtn = $("<button>").addClass("btn btn-danger").append("Delete");
 
    //Try to add a checkbox with the strike-thru like this?
-   var deleteTd = $("<td>").append(deleteBtn);
+   var deleteTd = $("<li>").append(deleteBtn);
 
    //add strikethru button on the add click, just like the delete button
    var strikeBtn = $("<button>").addClass("btn btn-success").append("Done");
-   var strikeTd = $("<td>").append(strikeBtn);
+   var strikeTd = $("<li>").append(strikeBtn);
 
 
    newRow.append(noteTd).append(deleteTd).append(strikeTd);
-   $("tbody").append(newRow);
+   $(".inline-editable").append(newRow);
 
    //empty newNote field
    $(".dataItem").val("").focus();
