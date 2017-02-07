16462fb47002c71f2d309166f93269e718d3addd
fosachiman <jasonphumphreys@gmail.com>
2017-02-07 17:18:31 -0500

    front-end complete for drop-down

--- public/javascripts/userpage.js
+++ public/javascripts/userpage.js
@@ -1,28 +1,40 @@
-//when a category is pulled in, it should have the category listed in the element
 
-//ADD
+let button = document.querySelectorAll('.add-button');
+button.forEach((button) => {
+  button.addEventListener('click', addClickHandler);
+})
 
-let input = document.querySelector('#add-item-text');
-let button = document.querySelector('#add-button');
-button.addEventListener('click', addClickHandler);
 let trashButton = document.querySelectorAll('.delete-button');
 trashButton.forEach((button) => {
   button.addEventListener('click', trashItem);
 })
 let listItem = document.querySelector('.list-item');
-listItem.addEventListener('change', addSubmitButton);
+  listItem.addEventListener('change', addSubmitButton);
 
 
-function addClickHandler() {
+function addClickHandler(e) {
+  //GOING DEEP INTO THE EVENT OBJECT lol
+  // console.log(e.srcElement.attributes.category.textContent)
+
   let newThing = document.createElement('div');
-  let list = document.querySelector('#item-list');
-  let addBox = document.querySelector('#add-item-text')
-  list.appendChild(newThing);
+  let list = document.querySelectorAll('.item-list');
+  let addBox = document.querySelectorAll('.add-item-text')
+  list.forEach((list) => {
+    if (list.getAttribute('category') === e.srcElement.attributes.category.textContent){
+      console.log(list.getAttribute('category'))
+      list.appendChild(newThing);
+    }
+  })
   let newItem = document.createElement('input');
+
+  // newThing.setAttribute('category', newThing.parentElement.category)
   newItem.setAttribute('class', 'newItem col s10 list-item');
   newItem.setAttribute('type', 'text');
   newItem.setAttribute('name', 'item');
-  newItem.setAttribute('value', addBox.value);
+  addBox.forEach((addBox) => {
+    if (addBox.getAttribute('category') === e.srcElement.attributes.category.textContent)
+      newItem.setAttribute('value', addBox.value);
+  })
   let trash = document.createElement('i');
   trash.setAttribute('class', 'material-icons col s1 delete-button');
   trash.setAttribute('id', 'delete-button');
@@ -35,14 +47,17 @@ function addClickHandler() {
   newThing.appendChild(trash);
   newThing.appendChild(desc);
   trash.addEventListener('click', trashItem);
-  newThing.setAttribute('style', 'background-color: green');
-  resetAddBox();
-  addSubmitButton();
+  resetAddBox(e);
+  addSubmitButton(e);
 }
 
-function resetAddBox() {
-  let addBox = document.querySelector('#add-item-text');
-  addBox.value = '';
+function resetAddBox(e) {
+  let addBox = document.querySelectorAll('.add-item-text');
+  addBox.forEach((addBox) => {
+    if (addBox.category === e.target.category)
+      addBox.value = '';
+  })
+
 }
 
 function trashItem(e) {
@@ -51,39 +66,10 @@ function trashItem(e) {
   addSubmitButton();
 }
 
-function addSubmitButton() {
-  let submitButtonCont = document.querySelector('.sub-button-cont');
-  submitButtonCont.setAttribute('style', 'display: block');
+function addSubmitButton(e) {
+  let submitButtonCont = document.querySelectorAll('.sub-button-cont');
+  submitButtonCont.forEach((sbc) => {
+    sbc.setAttribute('style', 'display: block');
+  })
 }
-
-// <ul class="collapsible box-thing" data-collapsible="accordion">
-//     <% categories.forEach((category) => {%>
-//     <li category="<%= category.dataValues.category %>" id="<%= category.dataValues.category %>">
-//       <div class="collapsible-header category-item"><%= category.dataValues.category %></div>
-//       <div class="collapsible-body row">
-//       <input id="add-item-text" type="text" placeholder="add a thing" class="add-field input-field col s10">
-//       <div class="col s2 add-button-cont">
-//        <a id="add-button" class="btn-floating btn-large waves-effect waves-light add-button"><i class="material-icons">add</i></a>
-//        </div>
-//       <form action="<%='/users/' + user.dataValues.id +'/' + category.dataValues.category %>" method="post">
-//         <ul id="item-list" class="<%= category.dataValues.category %> newItemField" tether="<%= category.dataValues.category %>">
-//           <% items.forEach((item) => { %>
-//           <% if (item.dataValues.category === category.dataValues.category && item.dataValues.name !== '') { %>
-//             <div>
-//               <input type="text" name="item" class="newItem col s10 list-item" id="<%= item.dataValues.name %>_input" value="<%= item.dataValues.name %>">
-//               <i class="material-icons col s1 delete-button" id="delete-button">delete</i>
-//               <i class="material-icons col s1 desc-button" id="desc-button">info</i>
-//             </div>
-//           <%  } }) %>
-//           <!-- <button class="btn waves-effect waves-light" type="submit" name="action">Save
-//             <i class="material-icons right">send</i>
-//           </button> -->
-//         </form>
-//         </ul>
-//       </div>
-//     </li>
-//     <% }) %>
-// </ul>
-
-// item description
 
